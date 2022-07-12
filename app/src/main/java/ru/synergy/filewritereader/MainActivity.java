698b48da5af1aceffada48535b7c03a907796fe9
VladislavGV <0377722@mail.ru>
2022-07-12 12:31:56 +0300

    File external writing finished successfully

--- app/src/main/java/ru/synergy/filewritereader/MainActivity.java
+++ app/src/main/java/ru/synergy/filewritereader/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.TextureView;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -24,16 +26,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private File getExternalFilePath(){                           // меняем место хранения файла
+        return new File(getExternalFilesDir(null), FILE_NAME);
+
+    }
+
+
     // сохранение файла
     public void saveText(View view) {
         FileOutputStream fos = null;
 
         try {
-            EditText textBox = (EditText) findViewById(R.id.editor);
-            String text = textBox.getText().toString();
-
-            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
-            fos.write(text.getBytes());
+            EditText textBox = (EditText) findViewById(R.id.editor); // находим поле EditText
+            String text = textBox.getText().toString(); // достаем текст, преобразуем в String
+            fos = new FileOutputStream(getExternalFilePath()); // открываем из места хранения файла
+        // fos = openFileOutput(FILE_NAME, MODE_PRIVATE); // находим сам файл, (имя файла, доступ - только владелец)
+            fos.write(text.getBytes()); // записываем байты текста, создаем массив байтов по размеру String
             Toast.makeText(this, "Файл успешно сохранен", Toast.LENGTH_SHORT).show();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -57,14 +65,16 @@ public class MainActivity extends AppCompatActivity {
     // открытие файла
     public void openText(View view) {
         FileInputStream fin = null;
-        TextView textView = (TextView) findViewById(R.id.text);
+        TextView textView = (TextView) findViewById(R.id.text); // находим поле для вывода текста
+        File file = getExternalFilePath();
 
         try {
-            fin = openFileInput(FILE_NAME);
-            byte[] bytes = new byte[fin.available()];
-            fin.read(bytes);
-            String text = new String(bytes);
-            textView.setText(text);
+              fin = new FileInputStream(file);
+        //    fin = openFileInput(FILE_NAME); // при помощи openFileInput создаем String для FILE_NAME
+            byte[] bytes = new byte[fin.available()]; // создаем массив по размеру нашего файла
+            fin.read(bytes); // читаем файл/массив байтов
+            String text = new String(bytes); // создаем новый String для нашего гового потока
+            textView.setText(text); // показ текста
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
